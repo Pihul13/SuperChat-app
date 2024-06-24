07367cab3c7ee25340c710f0208d86915b94c9e0
RohanGIT <rohankumarmahato345@gmail.com>
2024-06-24 19:53:56 +0530

    UI done & fixed bugs occuring on edge browser

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -1,5 +1,22 @@
 import "./chat.css"
+import { useEffect, useState, useRef } from "react";
+import EmojiPicker from "emoji-picker-react";
+
 const Chat = () => {
+const [open,setOpen]=useState(false);
+const [text,setText]=useState("");
+// for scrolling 
+const endRef=useRef(null); 
+
+useEffect(()=>{
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+},[]);
+
+const handleEmoji=(e)=>{
+    setText((prev)=>prev+e.emoji);
+    setOpen(false);
+};
+console.log(text);
     return (
         <div className='chat'>
             <div className="top">
@@ -16,8 +33,107 @@ const Chat = () => {
                     <img src="./info.png" alt=""/>
                 </div>
             </div>
-            <div className="center"></div>
-            <div className="bottom"></div>
+            <div className="center">
+{/* message break */}
+
+                <div className="message">
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="texts">
+                    {/* when an image is sent  */}
+                        <img src="./imagesent.jpg" alt=""/>
+                        <p>
+                            ehllo mello rohan is here
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+{/* message break  */}
+{/* to scroll initial  */}
+            <div ref={endRef}></div>
+
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt=""/>
+                    <img src="./camera.png" alt=""/>
+                    <img src="./mic.png" alt=""/>
+                </div>
+                <input type="text" placeholder="Type a message..." value={text} onChange={(e)=>setText(e.target.value)}/>
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" onClick={()=>setOpen(!open)}/>
+                    <div className="picker">
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
         </div>
     );
 }
--- src/detail/Detail.jsx
+++ src/detail/Detail.jsx
@@ -1,7 +1,92 @@
 import "./detail.css"
 const Detail = () => {
     return (
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt=""/>
+                <h2>Rohan Kumar</h2>
+                <p>this is the sender's description</p>
+            </div>
+            <div className="info">
+{/* option break  */}
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & Help</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photos</span>
+                        <img src="./arrowDown.png" alt=""/>
+                    </div>
+                    <div className="photos">
+        {/* photo item shared  */}
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="./imagesent.jpg" alt=""/>
+                            <span>Photo_2024.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="./imagesent.jpg" alt=""/>
+                            <span>Photo_2024.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="./imagesent.jpg" alt=""/>
+                            <span>Photo_2024.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="./imagesent.jpg" alt=""/>
+                            <span>Photo_2024.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+
+ {/* photo item shared  */}
+                    </div>
+                </div>
+
+                
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+{/* option break  */}
+
+                <button>Block User</button>
+                <button className="logout">Logout</button>
+            </div>
+        </div>
     );
 }
 

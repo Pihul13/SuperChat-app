1ddaed988ed177a26d053816bd87d6e23c61f531
RohanGIT <rohankumarmahato345@gmail.com>
2024-06-24 14:11:13 +0530

    added scroll to chat list and improved basic structure

--- src/list/chatList/ChatList.jsx
+++ src/list/chatList/ChatList.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode,setaddMode]=useState(true);
     return (
-        <div className='userInfo'>chatlist</div>
+        <div className='chatList'>
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt=""/>
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <img src={addMode?"./plus.png":"./minus.png"} alt="" className="add" onClick={()=>{setaddMode(!addMode)}}/>
+            </div>
+{/* break */}
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Rohan Kumar</span>
+                    <p>Hellothere</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Rohan Kumar</span>
+                    <p>Hellothere</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Rohan Kumar</span>
+                    <p>Hellothere</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Rohan Kumar</span>
+                    <p>Hellothere</p>
+                </div>
+            </div>
+{/* break */}
+        </div>
     );
 }
 
